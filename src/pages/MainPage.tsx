f7fb2cad9ccfa9f216f5905111772aa9b162cb95
Hm-source <swo8852@gmail.com>
2023-08-23 00:51:35 +0900

    feat: MainPage에서 productList 호출하도록 구현(fake api 사용)

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+import ProductList from '../components/ProductList';
+interface Product {
+	id: number;
+	title: string;
+	price: number;
+	image: string;
+	description: string;
+	category: string;
+}
+const MainPage = () => {
+	const [products, setProducts] = useState<Product[]>([]);
+
+	useEffect(() => {
+		const fetchProducts = async () => {
+			try {
+				const response = await fetch('https://fakestoreapi.com/products');
+				const data = await response.json();
+				setProducts(data);
+			} catch (error) {
+				console.error('Error fetching products:', error);
+			}
+		};
+
+		fetchProducts();
+	}, []);
+	return (
+		<>
+			<ProductList products={products} />
+		</>
+	);
+};
+
+export default MainPage;

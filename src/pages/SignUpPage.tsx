f5c3d450c8c7a47d3eaea40a043281a0947d503f
prgmr99 <johnyeom24@naver.com>
2023-08-24 21:49:42 +0900

    머지 전 커밋

--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const SignUpPage: React.FC = () => {
-	return <div></div>;
+	return <SSignUp></SSignUp>;
 };
 
 export default SignUpPage;
+
+const SSignUp = styled.div``;

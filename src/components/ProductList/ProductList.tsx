f3df1a46ed92bd24280d5934752c8462357ef8e3
Hm-source <swo8852@gmail.com>
2023-08-31 00:18:37 +0900

    main 에 sidebar 추가

--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -15,8 +15,8 @@ const ProductList: React.FC<ProductLists> = ({ products }: ProductLists) => {
 
 const Grid = styled.div`
 	display: grid;
-	grid-template-columns: repeat(auto-fill, minmax(200px, 1fr)); /* 칸 크기 설정 */
-	grid-template-rows: auto; /* 칸 높이 설정 */
+	grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+	grid-template-rows: auto;
 	grid-gap: 20px; /* 칸 사이의 간격 설정 */
 	margin-top: 70px;
 `;

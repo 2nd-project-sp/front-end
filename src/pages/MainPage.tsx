8232fa1e9f19bb6fb8c459fb61c3f288e417b0c5
hyomin <hm0704vip@incross.com>
2023-09-04 17:53:14 +0900

    useEffect 삭제

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import ProductList from '../components/ProductList/ProductList';
 import Sidebar from '../components/Sidebar';

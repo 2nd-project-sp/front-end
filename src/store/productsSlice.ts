07aec839afbb4a81f5fde7eda70feced6d0710fc
giogio165 <103351870+giogio165@users.noreply.github.com>
2023-09-05 19:27:12 +0900

    [feat] 옵션 선택기능 추가

--- src/store/productsSlice.ts
+++ src/store/productsSlice.ts
@@ -1,44 +1,33 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-// Define the initial state
+// 초기값
 const initialState = {
 	data: [],
 };
 
-// Create a products slice
 const productsSlice = createSlice({
 	name: 'products',
 	initialState,
 	reducers: {
-		// Reducer for storing fetched data
 		setProductsData: (state, action) => {
 			state.data = action.payload;
 		},
 	},
 });
 
-// Export the reducer
-export const { reducer: productsReducer } = productsSlice;
-
-// Export the action creator
-export const { setProductsData } = productsSlice.actions;
-
-// Define a function to fetch products (Thunk removed)
+export const { reducer: productsReducer, actions } = productsSlice;
+export const { setProductsData } = actions;
 export const fetchProducts = () => async dispatch => {
 	try {
-		// Fetch data from the API
 		const response = await fetch('https://dummyjson.com/products');
 		const data = await response.json();
 
-		// Update the state with the fetched data
 		dispatch(setProductsData(data));
 	} catch (error) {
-		// You can handle errors here if needed
 		console.error('Error fetching products:', error);
 	}
 };
 
-// Selector to get product data
 export const selectProductData = state => state.products.data;
 
 export default productsReducer;

55163db132224bd95d3337f4d7d9fb6ce8a39dc5
hyomin <hm0704vip@incross.com>
2023-09-07 17:01:52 +0900

    react-query devtools 추가

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './global.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 const queryClient = new QueryClient();
 import {
 	LoginPage,
@@ -56,5 +56,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 		<Provider store={store}>
 			<RouterProvider router={router} />
 		</Provider>
+		<ReactQueryDevtools />
 	</QueryClientProvider>
 );

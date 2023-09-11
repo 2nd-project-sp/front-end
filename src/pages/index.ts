7c733fd03da23d118f6528aba3070fdaf7de8517
giogio165 <103351870+giogio165@users.noreply.github.com>
2023-09-11 22:26:26 +0900

    에러 진짜 해결했음

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -3,11 +3,7 @@ import MyBagPage from './MyBagPage';
 import MyPage from './MyPage';
 import MainPage from './MainPage';
 import SignUpPage from './SignUpPage';
-<<<<<<< HEAD
 import ProductMarket from './ProductMarket';
-=======
-import ProductInput from './ProductInput';
->>>>>>> ca2d2ac57888d3822de41fe2bcbb25cd0cd6a6ea
 import PaymentPage from './PaymentPage';
 import ProductDetail from './DetailPage/ProductDetail';
 export {
@@ -15,11 +11,7 @@ export {
 	MyBagPage,
 	MyPage,
 	MainPage,
-<<<<<<< HEAD
 	ProductMarket,
-=======
-	ProductInput,
->>>>>>> ca2d2ac57888d3822de41fe2bcbb25cd0cd6a6ea
 	PaymentPage,
 	SignUpPage,
 	ProductDetail,

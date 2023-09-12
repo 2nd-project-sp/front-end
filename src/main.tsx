9508f51fe913d67aee2ee85c7e9314bcf1a9d3fc
giogio165 <103351870+giogio165@users.noreply.github.com>
2023-09-12 21:54:30 +0900

    마이너 충돌 해결

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
 import './global.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 const queryClient = new QueryClient();

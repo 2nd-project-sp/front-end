520e3bc73c00261e6d1f49047aef59b7f9a9c158
giogio165 <103351870+giogio165@users.noreply.github.com>
2023-09-14 00:36:09 +0900

    카테고리 이름 변경

--- src/pages/ProductMarket.tsx
+++ src/pages/ProductMarket.tsx
@@ -10,7 +10,7 @@ const ProductMarket: React.FC = () => {
 	const [quantity, setQuantity] = useState(1);
 	const [showDiscount, setShowDiscount] = useState(false);
 	const [isNew, setIsNew] = useState(false);
-
+	// console.log(isNew);
 	const option = ['WOMEN', 'MEN', 'DIGITAL', 'INTERIOR', 'BEAUTY', 'KIDS'];
 	const subOption = ['의류', '가방', '신발', '악세사리'];
 

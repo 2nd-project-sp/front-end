5bd33e99924a914ffa5d4d910de71da3e59d60ac
grace <kdjsanh0217@naver.com>
2023-09-15 01:16:07 +0900

    결제완료 페이지 UI 수정 및 가상계좌 영역 추가

--- src/pages/PaymentComplete.tsx
+++ src/pages/PaymentComplete.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 
@@ -15,18 +15,62 @@ const PaymentComplete: React.FC = () => {
     const location = useLocation();
     const formData: FormData | undefined = location.state?.formData;
 
+    const [virtualAccount, setVirtualAccount] = useState('');
+    const [paymentDate, setPaymentDate] = useState(new Date());
+    
+    const generateVirtualAccount = () => {
+        const accountNumber = Array.from({ length: 12 }, () => Math.floor(Math.random() * 10)).join('');
+        setVirtualAccount(accountNumber);
+    };
+
+     // 컴포넌트가 마운트될 때 가상 계좌 번호 생성
+     useEffect(() => {
+        generateVirtualAccount();
+        setPaymentDate(new Date());
+    }, []); // 빈 종속성 배열을 사용하여 컴포넌트 마운트 시에만 함수가 호출되도록 함
+
+
     return (
         <Container>
             <Title>COMPELETE ORDER</Title>
             {formData && (
                 <InfoCard>
-                    <InfoItem>{formData.name}님 주문이 완료되었습니다.</InfoItem>
-                    <InfoItem><strong>이름:</strong> {formData.name}</InfoItem>
-                    <InfoItem><strong>주소:</strong> {formData.address}</InfoItem>
-                    <InfoItem><strong>도시:</strong> {formData.city}</InfoItem>
-                    <InfoItem><strong>핸드폰 번호:</strong> {formData.phone}</InfoItem>
-                    <InfoItem><strong>Email:</strong> {formData.email}</InfoItem>
-                    <InfoItem><strong>배송 요청사항:</strong> {formData.shippingRequest}</InfoItem>
+                    <Table>
+                        <tbody>
+                            <TableRow>
+                                <TableHeader>이름:</TableHeader>
+                                <TableData>{formData.name}</TableData>
+                            </TableRow>
+                            <TableRow>
+                                <TableHeader>주소:</TableHeader>
+                                <TableData>{formData.address}</TableData>
+                            </TableRow>
+                            <TableRow>
+                                <TableHeader>도시:</TableHeader>
+                                <TableData>{formData.city}</TableData>
+                            </TableRow>
+                            <TableRow>
+                                <TableHeader>핸드폰 번호:</TableHeader>
+                                <TableData>{formData.phone}</TableData>
+                            </TableRow>
+                            <TableRow>
+                                <TableHeader>Email:</TableHeader>
+                                <TableData>{formData.email}</TableData>
+                            </TableRow>
+                            <TableRow>
+                                <TableHeader>배송 요청사항:</TableHeader>
+                                <TableData>{formData.shippingRequest}</TableData>
+                            </TableRow>
+                            <TableRow>
+                                <TableHeader>결제 일시:</TableHeader>
+                                <TableData>{`${paymentDate.getFullYear()}-${paymentDate.getMonth() + 1}-${paymentDate.getDate()}`}</TableData>
+                            </TableRow>
+                            <TableRow>
+                                <TableHeader>가상 계좌 번호:</TableHeader>
+                                <TableData>{virtualAccount}</TableData>
+                            </TableRow>
+                        </tbody>
+                    </Table>
                 </InfoCard>
             )}
         </Container>
@@ -50,9 +94,6 @@ const InfoCard = styled.div`
     box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.1);
 `;
 
-const InfoItem = styled.div`
-    margin-bottom: 15px;
-`;
 
 
 const Title = styled.h1`
@@ -63,6 +104,33 @@ const Title = styled.h1`
     border-bottom: 2px solid #333;
     padding-bottom: 10px;
 `;
+
+
+const Table = styled.table`
+    width: 100%;
+    border-collapse: collapse;
+`;
+
+const TableRow = styled.tr`
+    &:nth-child(odd) {
+        background-color: #f2f2f2;
+    }
+`;
+
+const TableHeader = styled.th`
+    padding: 8px;
+    text-align: left;
+    border-bottom: 1px solid #ddd;
+    width: 25%;
+`;
+
+const TableData = styled.td`
+    padding: 8px;
+    text-align: left;
+    border-bottom: 1px solid #ddd;
+    
+`;
+
 
 
 export default PaymentComplete;

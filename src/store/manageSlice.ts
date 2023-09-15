e135365795126e68f8a021604a54b3a8c7d0f4a7
giogio165 <103351870+giogio165@users.noreply.github.com>
2023-09-15 23:05:43 +0900

    에러해결

--- src/store/manageSlice.ts
+++ src/store/manageSlice.ts
@@ -1,9 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-<<<<<<< HEAD
 import { CustomProductInterface } from '../pages/ProductMarket';
-=======
-import { ProductLists } from '../models/product';
->>>>>>> c50928afbd2ca8b1554daff5b2b20da2c0db7bd3
 
 const initialState: CustomProductInterface[] = [];
 
@@ -15,13 +11,8 @@ const manageSlice = createSlice({
 	name: 'manage',
 	initialState,
 	reducers: {
-<<<<<<< HEAD
 		addToManage: (state, action: PayloadAction<CustomProductInterface>) => {
 			state.push(action.payload);
-=======
-		addToManage: (state, action) => {
-			state.products.push(action.payload);
->>>>>>> c50928afbd2ca8b1554daff5b2b20da2c0db7bd3
 		},
 		updateQuantity: (state, action: PayloadAction<{ productId: number; newQuantity: number }>) => {
 			const { productId, newQuantity } = action.payload;
